4bc15bf8922eb0b394f31c3366b4d8c51e1d09a4
Václav Kouřil <vaclav.kouril02@gmail.com>
2023-08-14 19:15:54 +0200

    Created interface, class and started with convertion of big number into vector

--- Long_Numbers/Long_Numbers/Program.cs
+++ Long_Numbers/Long_Numbers/Program.cs
@@ -2,6 +2,23 @@
 
 namespace Long_Numbers
 {
+    public interface LongNumber
+    {
+        public void LongNumber(string bigNum);
+        public LongNumber Plus(LongNumber num1, LongNumber num2);
+    }
+    public class Long_number : LongNumber
+    {
+        public void LongNumber(string bigNum)
+        {
+            
+        }
+        public LongNumber Plus(LongNumber a, LongNumber b)
+        {
+            Console.WriteLine("_");
+            return a;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
