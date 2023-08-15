3d6969cf8f65e970c6c2a4a8941ed5b9d13fe128
Václav Kouřil <vaclav.kouril02@gmail.com>
2023-08-15 23:17:51 +0200

    Overloaded plus operator to add vectors together

--- Long_Numbers/Long_Numbers/Program.cs
+++ Long_Numbers/Long_Numbers/Program.cs
@@ -24,17 +24,23 @@ namespace Long_Numbers
                 vector[i] = modulo(bigNum, Primes[i]);
             }
         }
-        public LongNumber Plus(LongNumber a, LongNumber b)
+        static public LongNumber operator + (LongNumber num1, LongNumber num2)
         {
-            Console.WriteLine("_");
-            return a;
+            LongNumber num3 = new LongNumber("0");
+            for (int i = 0; i < Primes.Length; i++)
+            {
+                num3.vector[i] = (num1.vector[i] + num2.vector[i]) % Primes[i];
+            }
+            return num3;
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            LongNumber a = new LongNumber("10000123378483919101091398485u75202");
+            LongNumber a = new LongNumber("10000123378483919101091398485123345765432765432543254325432543254324375202");
+            LongNumber b = new LongNumber("10000123378483919101091398485752022134932038410847104872109389028310");
+            LongNumber c = a + b;
             Console.ReadLine();
         }
     }
