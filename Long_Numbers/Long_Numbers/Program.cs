65474942fe485953cb6bef6b9f9be611c104a7d0
Václav Kouřil <vaclav.kouril02@gmail.com>
2023-08-16 19:15:50 +0200

    Added modulo outputing positive results, substraction and multiplication functions

--- Long_Numbers/Long_Numbers/Program.cs
+++ Long_Numbers/Long_Numbers/Program.cs
@@ -6,6 +6,11 @@ namespace Long_Numbers
     public class LongNumber
     {
         static int[] Primes = {2,3,5,7,1000012337};
+
+        private static int modulo(int dividend, int divisor)
+        {
+            return (dividend % divisor+divisor) % divisor;
+        }
         static int modulo(String num, int a)
         {
             int result = 0;
@@ -24,12 +29,31 @@ namespace Long_Numbers
                 vector[i] = modulo(bigNum, Primes[i]);
             }
         }
-        static public LongNumber operator + (LongNumber num1, LongNumber num2)
+        public static LongNumber operator + (LongNumber num1, LongNumber num2)
         {
             LongNumber num3 = new LongNumber("0");
             for (int i = 0; i < Primes.Length; i++)
             {
-                num3.vector[i] = (num1.vector[i] + num2.vector[i]) % Primes[i];
+                num3.vector[i] = modulo((num1.vector[i] + num2.vector[i]),Primes[i]);
+            }
+            return num3;
+        }
+        
+        public static LongNumber operator - (LongNumber num1, LongNumber num2)
+        {
+            LongNumber num3 = new LongNumber("0");
+            for (int i = 0; i < Primes.Length; i++)
+            {
+                num3.vector[i] = modulo((num1.vector[i] - num2.vector[i]),Primes[i]);
+            }
+            return num3;
+        }
+        public static LongNumber operator * (LongNumber num1, LongNumber num2)
+        {
+            LongNumber num3 = new LongNumber("0");
+            for (int i = 0; i < Primes.Length; i++)
+            {
+                num3.vector[i] = modulo((num1.vector[i] * num2.vector[i]),Primes[i]);
             }
             return num3;
         }
@@ -41,6 +65,8 @@ namespace Long_Numbers
             LongNumber a = new LongNumber("10000123378483919101091398485123345765432765432543254325432543254324375202");
             LongNumber b = new LongNumber("10000123378483919101091398485752022134932038410847104872109389028310");
             LongNumber c = a + b;
+            LongNumber d = a * b * c;
+            LongNumber e = (d - a) * c;
             Console.ReadLine();
         }
     }
