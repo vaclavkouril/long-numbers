09f360c0f01f70687c5fb2b888b1f2ed2928f59d
Václav Kouřil <vaclav.kouril02@gmail.com>
2023-08-23 20:17:05 +0200

    Created test1

---
+++ test/Long_Numbers/Test1_addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+using LongNumberLib;
+
+namespace Long_Numbers;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        LongNumber a = new LongNumber("37520224124164609");
+        LongNumber b = new LongNumber("53752412124742020");
+        LongNumber c = new LongNumber("89429210844762");
+        a += b + c;
+        Console.WriteLine(a.ToBigInteger().ToString());
+    }
+}
